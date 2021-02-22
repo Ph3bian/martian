7ad456efad4c568f94465f1c928d232b41340e64
phebian <phebian.chukwurah@chlela.com>
2021-02-22 12:16:03 +0100

    add more argtypes

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -4,6 +4,11 @@ import { Button } from "components";
 export default {
   title: "Button",
   component: Button,
+  parameters: {
+    actions: {
+      handles: ["mouseover", "click .btn"],
+    },
+  },
 };
 
 const Template = (args) => <Button {...args} />;
@@ -13,14 +18,20 @@ Primary.args = {
   variant: "primary",
   text: "Submit",
   type: "button",
-  onClick:()=>alert("You clicked me")
 };
- 
-export const  Neutral = Template.bind({});
- Neutral.args = {
-    variant: "neutral",
-    text: "Button",
-    type: "button",
-    onClick:()=>alert("You clicked me")
-  };
+Primary.argTypes = { onClick: { action: "clicked" } };
+export const Neutral = Template.bind({});
+Neutral.args = {
+  variant: "neutral",
+  text: "Button",
+  type: "button",
+  onClick: () => alert("You clicked me"),
+};
 
+export const Submit = Template.bind({});
+Submit.args = {
+  variant: "primary",
+  text: "Button",
+  type: "submit",
+};
+Submit.argTypes = { handleClick: { action: 'submitted' } };
